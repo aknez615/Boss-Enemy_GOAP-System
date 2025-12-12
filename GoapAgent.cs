f801bc76b88d318dc2a71eb66f925a58183217a5
Alyssa Knez <al683193@ucf.edu>
2025-12-12 14:28:13 -0500

    recommit

--- GoapAgent.cs
+++ GoapAgent.cs
@@ -5,29 +5,19 @@ using UnityEngine.AI;
 
 
 [RequireComponent (typeof(NavMeshAgent))]
-public class GoapAgent : MonoBehaviour
+public partial class GoapAgent : MonoBehaviour
 {
     [Header("Sensors")]
     [SerializeField] Sensor chaseSensor; //bigger radius
-    [SerializeField] Sensor attackSensor;
+    [SerializeField] public Sensor attackSensor;
 
-    [Header("Locations")]
-    [SerializeField] Transform restingPosition;
-    [SerializeField] Transform hidingPosition;
-
-    [Header("Stats")] //temporary implementation
-    public float health = 75f;
-
-    [Header("Player Information")]
+    [Header("Player Reference")]
     [SerializeField] private GameObject player;
     public GameObject Player => player;
-
-    NavMeshAgent navMesh;
-    Rigidbody rb;
-
     GameObject target;
     Vector3 destination;
 
+    //Goap core information
     CountdownTimer timer;
 
     AgentGoal lastGoal;
@@ -39,135 +29,50 @@ public class GoapAgent : MonoBehaviour
     public HashSet<AgentAction> actions;
     public HashSet<AgentGoal> goals;
 
-    private bool resurrectedInPhaseOne = false;
-    private bool resurrectedInPhaseTwo = false;
-    public bool canResurrect => EvaluateResurrection();
-
     IGoapPlanner gPlanner;
-    internal float damage;
+    IGoapBehaviour behaviour;
+
+    private NavMeshAgent navMesh;
+    public NavMeshAgent NavMesh => navMesh;
+
+    private bool isActive = false;
 
     private void Awake()
     {
         navMesh = GetComponent<NavMeshAgent>();
-        rb = GetComponent<Rigidbody>();
+        /*rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
 
+        mechromancer = GetComponent<Mechromancer>();*/
         gPlanner = new GoapPlanner();
     }
 
     private void Start()
     {
         SetupTimers();
-        SetupBeliefs(); //Beliefs have to come first because actions and goals depend on beliefs
-        SetupActions();
-        SetupGoals();
+        //Beliefs have to come first because actions and goals depend on beliefs
+        
+        behaviour = GetComponent<IGoapBehaviour>();
+
+        if (behaviour == null)
+        {
+            Debug.LogError("Missing IGoapBehaviour on agent");
+            enabled = false;
+            return;
+        }
+
+        beliefs = behaviour.ProvideBeliefs();
+        actions = behaviour.ProvideActions(beliefs);
+        goals = behaviour.ProvideGoals(beliefs);
+
+        chaseSensor.OnTargetChanged += HandleTargetChanged;
     }
 
-    void SetupBeliefs()
+    private void OnDestroy()
     {
-        beliefs = new Dictionary<string, AgentBelief>();
-        BeliefFactory factory = new BeliefFactory(this, beliefs);
-
-        factory.AddBelief("Nothing", () => false);
-
-        factory.AddBelief("AgentIdle", () => !navMesh.hasPath);
-        factory.AddBelief("AgentMoving", () => navMesh.hasPath);
-        //factory.AddBelief("MovingToAttack", () => navMesh.hasPath);
-
-        factory.AddBelief("PhaseOne", () => health >= 50);
-        factory.AddBelief("PhaseTwo", () => health < 50 && health >= 35);
-        factory.AddBelief("Rage", () => health < 35);
-
-        factory.AddLocationBelief("AgentAtHidingPosition", 8f, hidingPosition);
-        factory.AddLocationBelief("AgentAtRestingPosition", 3f, restingPosition);
-
-        factory.AddSensorBelief("PlayerInChaseRange", chaseSensor);
-        factory.AddSensorBelief("PlayerInAttackRange", attackSensor);
-
-        factory.AddBelief("AttackingPlayer", () => false); //Player can always be attacked, will never come true
-        factory.AddBelief("CanResurrect", () => canResurrect);
-        factory.AddBelief("HasResurrectedThisPhase", () => !canResurrect);
-    }
-
-    void SetupActions()
-    {
-        actions = new HashSet<AgentAction>();
-
-        actions.Add(new AgentAction.Builder("Relax")
-            .WithStrategy(new IdleStrategy(5))
-            .AddEffect(beliefs["Nothing"])
-            .Build());
-
-        actions.Add(new AgentAction.Builder("Wander Around")
-            .WithStrategy(new WanderStrategy(navMesh, 10))
-            .AddEffect(beliefs["AgentMoving"])
-            .Build());
-
-        actions.Add(new AgentAction.Builder("Move To Hiding Position")
-            .WithStrategy(new MoveStrategy(navMesh, () => hidingPosition.position))
-            .AddEffect(beliefs["AgentAtHidingPosition"])
-            .Build());
-
-        actions.Add(new AgentAction.Builder("Hiding To Rest Area")
-            .WithStrategy(new MoveStrategy(navMesh, () => restingPosition.position))
-            .AddPrecondition(beliefs["AgentAtHidingPosition"])
-            .AddEffect(beliefs["AgentAtRestingPosition"])
-            .Build());
-
-        actions.Add(new AgentAction.Builder("Scan")
-            .WithStrategy(new WanderStrategy(navMesh, 10))
-            .AddPrecondition(beliefs["AgentAtRestingPosition"])
-            .AddEffect(beliefs["AgentMoving"])
-            .Build());
-
-        actions.Add(new AgentAction.Builder("Chase Player")
-            .WithStrategy(new MoveStrategy(navMesh, () => beliefs["PlayerInChaseRange"].Location))
-            .AddPrecondition(beliefs["PlayerInChaseRange"])
-            .AddEffect(beliefs["PlayerInAttackRange"])
-            .Build());
-
-        actions.Add(new AgentAction.Builder("Attack Player")
-            .WithStrategy(new AttackStrategy(this))
-            .AddPrecondition(beliefs["PlayerInAttackRange"])
-            .AddEffect(beliefs["AttackingPlayer"])
-            .Build());
-
-        actions.Add(new AgentAction.Builder("Resurrect Robots")
-            .WithStrategy(new ResurrectStrategy(this.gameObject))
-            .AddPrecondition(beliefs["AgentAtHidingPosition"])
-            .AddPrecondition(beliefs["CanResurrect"])
-            .AddEffect(beliefs["HasResurrectedThisPhase"])
-            .Build());
-    }
-
-    void SetupGoals()
-    {
-        goals = new HashSet<AgentGoal>();
-
-        goals.Add(new AgentGoal.Builder("Chill out")
-            .WithPriority(1)
-            .WithDesiredEffect(beliefs["Nothing"])
-            .Build());
-
-        goals.Add(new AgentGoal.Builder("Wander")
-            .WithPriority(1)
-            .WithDesiredEffect(beliefs["AgentMoving"])
-            .Build());
-
-        goals.Add(new AgentGoal.Builder("Hide")
-            .WithPriority(2)
-            .WithDesiredEffect(beliefs["AgentAtHidingPosition"])
-            .Build());
-
-        goals.Add(new AgentGoal.Builder("SeekAndKill")
-            .WithPriority(3)
-            .WithDesiredEffect(beliefs["AttackingPlayer"])
-            .Build());
-
-        goals.Add(new AgentGoal.Builder("Resurrect")
-            .WithPriority(5)
-            .WithDesiredEffect(beliefs["HasResurrectedThisPhase"])
-            .Build());
+        chaseSensor.OnTargetChanged -= HandleTargetChanged;
     }
 
     void SetupTimers() //currently timers are working to change the health bar, for the final build we do not want this as the enemy should only be taking damage from the player REMOVE SOON
@@ -175,16 +80,9 @@ public class GoapAgent : MonoBehaviour
         timer = new CountdownTimer(2f);
         timer.OnTimerStop += () =>
         {
-            UpdateStats();
             timer.Start();
         };
         timer.Start();
-    }
-
-    void UpdateStats()
-    {
-        health += InRangeOf(restingPosition.position, 3f) ? 20 : -10;
-        health = Mathf.Clamp(health, 0, 75);
     }
 
     bool InRangeOf(Vector3 pos, float range) => Vector3.Distance(transform.position, pos) <= range;
@@ -192,9 +90,23 @@ public class GoapAgent : MonoBehaviour
     void OnEnable() => chaseSensor.OnTargetChanged += HandleTargetChanged;
     void OnDisable() => chaseSensor.OnTargetChanged -= HandleTargetChanged;
 
-    void HandleTargetChanged() //force planner to change if things aren't going to plan
+    public void HandleTargetChanged() //force planner to change if things aren't going to plan
     {
         Debug.Log("Target changed, clearing action and goal");
+
+        currentAction?.Stop();
+        currentAction = null;
+        currentGoal = null;
+        actionPlan = null;
+        lastGoal = null;
+        CalculatePlan();
+    }
+
+    public void ClearCurrentAction()
+    {
+        Debug.Log("Aborting current action and resetting goal");
+
+        currentAction?.Stop();
         currentAction = null;
         currentGoal = null;
     }
@@ -202,6 +114,7 @@ public class GoapAgent : MonoBehaviour
     private void Update()
     {
         //StatsTimer and Animations update
+        if (!isActive) return;
 
         //Update the plan and current action if there is one
         if (currentAction == null)
@@ -253,7 +166,7 @@ public class GoapAgent : MonoBehaviour
         }
     }
 
-    void CalculatePlan()
+    public void CalculatePlan()
     {
         var priorityLevel = currentGoal?.Priority ?? 0;
 
@@ -273,33 +186,26 @@ public class GoapAgent : MonoBehaviour
         }
     }
 
-    bool EvaluateResurrection()
+    public void EnableOnlyThisGoal(AgentGoal forcedGoal)
     {
-        if (health > 50) //phase one
+        foreach (var g in goals)
         {
-            return !resurrectedInPhaseOne;
+            g.Enabled = (g == forcedGoal);
         }
 
-        else if (health > 35) //phase two
-        {
-            return !resurrectedInPhaseTwo;
-        }
-
-        else //rage
-        {
-            return false;
-        }
+        currentGoal = forcedGoal;
+        CalculatePlan();
     }
 
-    public void MarkResurrected()
+    public void Activate()
     {
-        if (health > 50)
-        {
-            resurrectedInPhaseOne = true;
-        }
-        else if (health > 35)
-        {
-            resurrectedInPhaseTwo = true;
-        }
+        isActive = true;
+        Debug.Log($"{name}: GOAP Agent activated");
+    }
+
+    public void Deactivate()
+    {
+        isActive = false;
+        Debug.Log($"{name}: GOAP Agent deactivated");
     }
 }
--- Strategies.cs
+++ Strategies.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.ComponentModel;
-using UnityEditor.Rendering;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -25,6 +27,17 @@ public interface IActionStrategy
     }
 }
 
+public class IntroStrategy : IActionStrategy
+{
+    public bool CanPerform => false; //Can only be achieved after player is noticed
+    public bool Complete { get; private set; } //set complete after intro scene is played
+
+    public IntroStrategy()
+    {
+        //put in cinematic information
+    }
+}
+
 public class IdleStrategy : IActionStrategy
 {
     public bool CanPerform => true; //Agent can always idle
@@ -41,6 +54,26 @@ public class IdleStrategy : IActionStrategy
 
     public void Start() => timer.Start();
     public void Update(float deltaTime) => timer.Tick(deltaTime);
+
+    //Idle animations
+    //Need to add animation state machine in order to get this to work
+    //Save for next SCRUM
+    /*
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+    */
 }
 
 public class WanderStrategy : IActionStrategy
@@ -93,16 +126,26 @@ public class MoveStrategy : IActionStrategy
 
 public class AttackStrategy : IActionStrategy
 {
-    public bool CanPerform => true; //agent can always attack
+    public bool CanPerform => true;
     public bool Complete {  get; private set; }
 
-    readonly GoapAgent agent;
-    readonly float attackDuration = 1.5f;
-    readonly CountdownTimer timer;
+    private readonly IDamage damageProvider;
+    private readonly GoapAgent agent;
+    private Mechromancer mechromancer;
+    private MechAnimationController animationController;
+    private readonly NavMeshAgent navMesh;
+    private readonly float attackDuration = 1.5f;
+    private readonly CountdownTimer timer;
 
     public AttackStrategy(GoapAgent agent)
     {
         this.agent = agent;
+        this.navMesh = agent.GetComponent<NavMeshAgent>();
+
+        this.damageProvider = agent.GetComponent<IDamage>();
+        this.mechromancer = agent.GetComponent<Mechromancer>();
+        this.animationController = agent.GetComponent<MechAnimationController>();
+
         timer = new CountdownTimer(attackDuration);
         timer.OnTimerStart += () => Complete = false;
         timer.OnTimerStop += () =>
@@ -112,8 +155,81 @@ public class AttackStrategy : IActionStrategy
         };
     }
 
-    public void Start() => timer.Start();
-    public void Update(float deltaTime) => timer.Tick(deltaTime);
+    public void Start()
+    {
+        Debug.Log("Starting attack strategy");
+
+        navMesh.isStopped = true;
+        navMesh.updateRotation = false;
+
+        if (animationController != null)
+        {
+            animationController.SetIsAttacking(true);
+        }
+
+        timer.Start();
+    }
+
+    public void Update(float deltaTime)
+    {
+        float distanceToPlayer = Vector3.Distance(agent.transform.position, agent.Player.transform.position);
+        float attackRange = agent.attackSensor.detectionRadius;
+
+        if (distanceToPlayer < attackRange)
+        {
+            Debug.Log("Player moved out of attack range. Aborting attack");
+            AbortAttack();
+            return;
+        }
+
+        timer.Tick(deltaTime);
+
+        if (agent.Player != null)
+        {
+            Vector3 direction = (agent.Player.transform.position - agent.transform.position).normalized;
+            direction.y = 0f;
+
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                agent.transform.rotation = Quaternion.Slerp(agent.transform.rotation, targetRotation, deltaTime * 5f);
+            }
+        }
+
+        if (agent.Player == null)
+        {
+            Debug.LogWarning("No player assigned, aborting attack");
+            AbortAttack();
+            return;
+        }
+    }
+
+    public void Stop()
+    {
+        if (animationController != null)
+        {
+            animationController.SetIsAttacking(false);
+        }
+
+        navMesh.isStopped = false;
+        navMesh.updateRotation = true;
+    }
+
+    void AbortAttack()
+    {
+        timer.Stop();
+        Complete = true;
+
+        if (animationController != null)
+        {
+            animationController.SetIsAttacking(false);
+        }
+
+        navMesh.isStopped = false;
+        navMesh.updateRotation = true;
+
+        agent.ClearCurrentAction();
+    }
 
     void DealDamage()
     {
@@ -130,9 +246,109 @@ public class AttackStrategy : IActionStrategy
             return;
         }
 
-        playerHealth.TakeDamage(agent.damage);
-        Debug.Log($"Boss dealt {agent.damage} damage to the player");
+        if (playerHealth != null)
+        {
+            //float damage = damageProvider != null ? damageProvider.GetDamage() : 8f;
+            //playerHealth.TakeDamage(damage);
+            //Debug.Log($"Boss dealt {damage} damage to the player");
+        }
     }
+}
+
+public class LightningAttackStrategy : IActionStrategy
+{
+    public bool CanPerform => true;
+    public bool Complete {  get; private set; }
+
+    private readonly GoapAgent agent;
+    private readonly NavMeshAgent navMesh;
+    private readonly IDamage damageProvider;
+    private readonly LightningController lightningController;
+    private readonly CountdownTimer timer;
+
+    private readonly float attackDuration = 2.5f;
+
+    private Mechromancer mechromancer;
+    private Vector3 targetPosition;
+    private bool lightningFired = false;
+
+    public LightningAttackStrategy(GoapAgent agent, LightningController lightningController)
+    {
+        this.agent = agent;
+        this.navMesh = agent.GetComponent<NavMeshAgent>();
+        this.damageProvider = agent.GetComponent<IDamage>();
+        this.mechromancer = agent.GetComponent<Mechromancer>();
+        this.lightningController = lightningController;
+
+        timer = new CountdownTimer(attackDuration);
+        timer.OnTimerStart += () => Complete = false;
+        timer.OnTimerStop += () =>
+        {
+            Complete = true;
+            lightningController.StopLightning();
+        };
+    }
+
+    public void Start()
+    {
+        Debug.Log("Starting lightning strategy");
+        navMesh.isStopped = true;
+
+        if (agent.Player == null)
+        {
+            Debug.LogWarning("No player found, cannot cast lightning. Check inspector");
+            Complete = true;
+            return;
+        }
+
+        targetPosition = agent.Player.transform.position;
+
+        //animation
+        //mechromancer?.TriggerLightningAnimation();
+
+        timer.Start();
+
+        float castDelay = 0.75f; //so boss can raise their hand
+        agent.StartCoroutine(DelayedLightningCast(castDelay));
+    }
+
+    private System.Collections.IEnumerator DelayedLightningCast(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        FireLightning();
+    }
+
+    private void FireLightning()
+    {
+        if (lightningFired) return;
+
+        lightningFired = true;
+        //lightningController.CastLightning(targetPosition);
+        Debug.Log("Lightning casted toward {targetPosition}");
+    }
+
+    public void Update(float deltaTime)
+    {
+        timer.Tick(deltaTime);
+
+        if (agent.Player != null && !Complete)
+        {
+            Vector3 direction = (agent.Player.transform.position - agent.transform.position).normalized;
+            direction.y = 0f;
+            if (direction != Vector3.zero)
+            {
+                agent.transform.rotation = Quaternion.Slerp(agent.transform.rotation, Quaternion.LookRotation(direction), deltaTime * 3f);
+            }
+        }
+    }
+
+    public void Stop()
+    {
+        lightningController.StopLightning();
+        navMesh.isStopped = false;
+        Complete = true;
+    }
+
 }
 
 public class ResurrectStrategy : IActionStrategy
@@ -140,15 +356,26 @@ public class ResurrectStrategy : IActionStrategy
     public bool CanPerform => true;
     public bool Complete { get; private set; }
 
+    readonly MechromancerBehaviour mechromancer;
     readonly GoapAgent agent;
+    readonly GameObject enemyPrefab;
+    readonly Transform[] spawnPoints;
+    readonly Transform hidingPos;
     readonly float resurrectionTime = 5f;
     CountdownTimer timer;
 
     bool hasStarted = false;
+    bool movingToHiding = false;
 
-    public ResurrectStrategy(GameObject boss)
+    public ResurrectStrategy(GoapAgent agent, GameObject enemyPrefab, Transform[] spawnPoints, Transform hidingPos)
     {
-        this.agent = boss.GetComponent<GoapAgent>();
+        this.agent = agent;
+        this.enemyPrefab = enemyPrefab;
+        this.spawnPoints = spawnPoints;
+        this.hidingPos = hidingPos;
+
+        mechromancer = agent.GetComponent<MechromancerBehaviour>();
+
         timer = new CountdownTimer(resurrectionTime);
         timer.OnTimerStart += () => Complete = false;
         timer.OnTimerStop += () =>
@@ -163,14 +390,78 @@ public class ResurrectStrategy : IActionStrategy
         if (hasStarted) return; //prevent it happening multiple times
         hasStarted = true;
 
-        timer.Start();
-        agent.MarkResurrected();
+        if (Vector3.Distance(agent.transform.position, hidingPos.position) > 1f)
+        {
+            movingToHiding = true;
+            agent.StartCoroutine(MoveToHidingCoroutine());
+        }
+        else
+        {
+            timer.Start();
+            mechromancer?.MarkResurrected();
+        }
     }
 
-    public void Update(float deltaTime) => timer.Tick(deltaTime);
+    public void Update(float deltaTime)
+    {
+        if (movingToHiding) return;
+        timer.Tick(deltaTime);
+    }
+
+    private IEnumerator MoveToHidingCoroutine()
+    {
+        var nav = agent.GetComponent<NavMeshAgent>();
+        nav.SetDestination(hidingPos.position);
+
+        while (Vector3.Distance(agent.transform.position, hidingPos.position) > 1f)
+        {
+            yield return null;
+        }
+
+        movingToHiding = false;
+        timer.Start();
+        mechromancer.MarkResurrected();
+    }
 
     void ResurrectRobots()
     {
         Debug.Log("Summoning minions");
+
+        foreach (Transform spawnPoint in spawnPoints)
+        {
+            GameObject minion = UnityEngine.Object.Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+            Debug.Log($"Spawned enemy at {spawnPoint.name}");
+        }
+    }
+}
+
+public class DropDownStrategy : IActionStrategy
+{
+    public bool CanPerform => true;
+    public bool Complete { get; private set; }
+
+    readonly Transform boss;
+    readonly Vector3 groundPosition;
+    readonly float dropSpeed = 7f;
+
+    public DropDownStrategy(Transform boss, Vector3 dropPosition)
+    {
+        this.boss = boss;
+        this.groundPosition = dropPosition;
+    }
+
+    public void Start()
+    {
+        Complete = false;
+    }
+
+    public void Update(float deltaTime)
+    {
+        boss.position = Vector3.MoveTowards(boss.position, groundPosition, dropSpeed * deltaTime);
+        if (Vector3.Distance(boss.position, groundPosition) < 0.2f)
+        {
+            Complete = true;
+            Debug.Log("Boss landed for attack");
+        }
     }
 }
